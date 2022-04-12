37c79ead664c6d937209fa453fbecbcc451b10a7
LEX <zhang13pro@qq.com>
2022-04-12 21:53:28 +0800

    chore: add user with pinia

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,74 @@
 <template lang="pug">
-h1 Kobe Bryant
-p Do
-div Something Cool
-nuxt-link(to="/home") toHome  
-button +
-| {{counter}}
-button -
+main
+  .places(ref="places")
+    .location(v-for="place in places" :key="place.name")
+      img(:src="place.img" :alt="place.name")
+      h2 {{ place.name }}
+      p
+        strong Rating: {{ place.rating }}
+      p {{ place.description }}
+      hr
+  .mapcontain(ref="mapcontain")
+    p
+      icon-base(icon-name="mappin")
+        icon-map-pin
+      | Checked in at Honolulu location
+    img(src="map.png" alt="Map of Honolulu")
 </template>
+
 <script setup lang="ts">
-import { useCounterStore } from "../stores/counter"
-// TODO How does count is?
-const counter = useCounterStore()
-counter.count++
-// with autocompletion ✨
-counter.$patch({ count: counter.count + 1 })
-// or using an action instead
-counter.increment()
+const places = ref(["Hangzhou", "Shanghai", "Xian"])
+// computed: mapState(["page", "users", "places"]),
 </script>
+
+<style>
+.mapboxgl-missing-css {
+  display: none;
+}
+</style>
+
+<style lang="scss" scoped>
+main {
+  padding-top: 20px;
+  border-top: 1px solid #ddd;
+  margin-top: 120px;
+}
+
+.places {
+  img {
+    float: left;
+    margin: 0 15px 15px 0;
+  }
+  p {
+    margin-top: 10px;
+  }
+  .location {
+    padding: 10px 0;
+  }
+}
+
+hr {
+  border-top: 1px solid #ddd;
+  border-bottom: none;
+  margin-top: 15px;
+}
+
+.mapcontain {
+  width: 35%;
+  float: right;
+  height: 400px;
+  p {
+    margin: 10px 0;
+  }
+  img {
+    width: 100%;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .mapcontain {
+    width: 100%;
+    margin: 10px 0;
+  }
+}
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+h1 Kobe Bryant
+p Do
+div Something Cool
+button +
+div {{ counter.count }}
+button -
+</template>
+<script setup lang="ts">
+import { useCounterStore } from "../stores/counter"
+// TODO Why does count is?
+const counter = useCounterStore()
+counter.count++
+// with autocompletion ✨
+counter.$patch({ count: counter.count + 1 })
+// or using an action instead
+counter.increment()
+</script>
